a354719c042d2b12448f1b3e58bb13486d1f951d
BOBsoft <bobsoft_spb@mail.ru>
2022-08-30 13:46:24 +0300

    DZ4

---
+++ DZ4_1/Program.cs
@@ -0,0 +1,11 @@
+﻿// Задача 25: Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+// 3, 5 -> 243 (3⁵)
+// 2, 4 -> 16
+
+Console.WriteLine("Введите число:");
+int num1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите степень:");
+int num2 = Convert.ToInt32(Console.ReadLine());
+
+double result = Math.Pow(num1, num2);
+Console.WriteLine(num1 + "," + num2 + " -> " + result);
---
+++ DZ4_2/Program.cs
@@ -0,0 +1,33 @@
+﻿// Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+
+Console.WriteLine("Введите число:");
+int num = int.Parse(Console.ReadLine());
+Console.WriteLine($"Сумма цифр = {GetSumNums(num)}");
+
+int GetSumNums(int number)
+{
+    int sum = 0;
+    while(number>0)
+    {
+        sum+=number%10;
+        number/=10;
+    }
+    return sum;
+}
+
+// почему-то не работает:  ((((
+// int num = Convert.ToInt32(Console.ReadLine());
+// string result = Convert.ToString(num);
+// int index = 0;
+// int summa = 0;
+
+// while(index < result.Length)
+// {
+//     summa = summa + Convert.ToInt32(result[index]);
+//     index++;
+// }
+// Console.WriteLine(num + " -> " + summa);
+
